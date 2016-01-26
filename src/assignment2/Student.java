3a6ed3b6d444758bf0cde270c84527fb9df6e98c
c0640780 <c0640780@SB-N208-35.lambton.on.ca>
2016-01-26 13:01:56 -0500

    Do this again since it didn't push correctly the first time.

---
+++ src/assignment2/Student.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+/**
+ *
+ * @author c0640780
+ */
+public class Student {
+    public String name;
+    public String id;
+    public String gender;
+    public double grade;
+    
+    public static void Student(){
+        
+    }
+    public static void Student(String name, String id, String gender, double grade){
+        
+    }
+    public static void getName(){
+        
+    }
+    public static void getId(){
+        
+    }
+    public static void getGender(){
+        
+    }
+    public static void getGrade(){
+        
+    }
+    public static void setName(String name){
+        
+    }
+    public static void setId(String id){
+        
+    }
+    public static void setGender(String gender){
+        
+    }
+    public static void setGrade (String grade){
+    
+    }
+    public boolean equals(Object obj){
+        return true;
+    }
+    public String toString(){
+       return "0";
+    }
+}
