52e44619e9c97f8203ffdfbe2cd451e7102b65db
'T.J. <c0640785@mylambton.ca>
2016-01-26 14:14:07 -0500

    Initial Commit

---
+++ test/assignment2/CourseTest.java
@@ -0,0 +1,175 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author c0640785
+ */
+public class CourseTest {
+    
+    public CourseTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of Course method, of class Course.
+     */
+    @Test
+    public void testCourse() {
+        System.out.println("Course");
+        Course.Course();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of <error> method, of class Course.
+     */
+    @Test
+    public void testadd() {
+        System.out.println("<error>");
+        Student student = null;
+        Course.add(student);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of remove method, of class Course.
+     */
+    @Test
+    public void testRemove() {
+        System.out.println("remove");
+        String id = "";
+        Course.remove(id);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of insert method, of class Course.
+     */
+    @Test
+    public void testInsert() {
+        System.out.println("insert");
+        Student student = null;
+        int position = 0;
+        Course.insert(student, position);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of get method, of class Course.
+     */
+    @Test
+    public void testGet_String() {
+        System.out.println("get");
+        String id = "";
+        Course.get(id);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of get method, of class Course.
+     */
+    @Test
+    public void testGet_int() {
+        System.out.println("get");
+        int position = 0;
+        Course.get(position);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAll method, of class Course.
+     */
+    @Test
+    public void testGetAll() {
+        System.out.println("getAll");
+        Course.getAll();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of equals method, of class Course.
+     */
+    @Test
+    public void testEquals() {
+        System.out.println("equals");
+        Object obj = null;
+        Course instance = new Course();
+        boolean expResult = false;
+        boolean result = instance.equals(obj);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toString method, of class Course.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Course instance = new Course();
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAllByGender method, of class Course.
+     */
+    @Test
+    public void testGetAllByGender() {
+        System.out.println("getAllByGender");
+        String gender = "";
+        Course.getAllByGender(gender);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getGradeMap method, of class Course.
+     */
+    @Test
+    public void testGetGradeMap() {
+        System.out.println("getGradeMap");
+        Course.getGradeMap();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
